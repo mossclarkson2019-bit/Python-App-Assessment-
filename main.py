81c77874b32da3e84462ccd6a001cd58c987f433
mossclarkson2019-bit <moss.clarkson2019@gmail.com>
2026-08-19 09:22:22 +1200

    creating first button and adding some data

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 #Imports
 import tkinter as tk
 import json
+from turtle import title
 
+#loading the data from the JSON files
 
-with open("data/vehicles.json", "r") as file:
-    vehicles = json.load(file)
+with open("databases/vehicles.json", "r") as file:
+    vehicles = json.load(file)                 # used gemini seach assist to fully understand the sytax and what is and isnt needed
 
-with open("data/parts.json", "r") as file:
+with open("databases/parts.json", "r") as file:
     parts = json.load(file)
 
 
 # Creating the main window
 root = tk.Tk()
 root.title("EuroPartPicker")
+
+
+
+
+
+
+
+
+
+select_button = tk.Button(
+    root,
+    text="SELECT PROJECT",
+    width=25,
+    height=3,
+)
+
+select_button.pack(pady=10)
+
+
+
+
+
+
 
 # Creating the event loop to keep the window visible
 root.mainloop()
